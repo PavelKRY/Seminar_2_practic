b8a3e8beb5732a30102fa9bd71c60e70ebe49b2c
PavelKRY <kryukov.p14@gmail.com>
2022-08-24 17:55:52 +0500

    Task 2 Issue

---
+++ Seminar_02_Task_02/Program.cs
@@ -0,0 +1,21 @@
+﻿int firstDiggit = new Random().Next(1,65537);
+
+Console.WriteLine(firstDiggit);
+
+if(firstDiggit > 9999)
+{
+    Console.WriteLine((firstDiggit/100)%10);
+}
+
+else if(10000 > firstDiggit && firstDiggit > 999)
+{
+  Console.WriteLine((firstDiggit/10)%10);
+}
+else if(1000 > firstDiggit && firstDiggit >99)
+{
+   Console.WriteLine(firstDiggit%10);
+}
+else
+{
+    Console.WriteLine("Diggit don't have 3 diggit");
+}
