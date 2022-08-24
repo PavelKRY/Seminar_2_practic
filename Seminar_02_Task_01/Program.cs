a9469ea68e8ca7368d97c207c4c87acf3584b245
PavelKRY <kryukov.p14@gmail.com>
2022-08-24 15:40:52 +0500

    Task 1 Issue

---
+++ Seminar_02_Task_01/Program.cs
@@ -0,0 +1,4 @@
+﻿int firstDiggit = new Random().Next(100,1000);
+
+int secondDiggit = (firstDiggit/10)%10;
+Console.WriteLine(secondDiggit + " - Второе число случайно сгенерированного трёхзначного числа");
