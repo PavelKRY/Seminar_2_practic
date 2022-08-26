82df2dcd9209e9f19fe61c96c725f476680749d4
PavelKRY <kryukov.p14@gmail.com>
2022-08-26 14:33:33 +0500

    Task 02_02 issue

---
+++ Seminar_02_Task_02_02/Program.cs
@@ -0,0 +1,19 @@
+﻿int firstDiggit = new Random().Next(1,65586796);
+int threeDiggit = firstDiggit;
+Console.WriteLine(threeDiggit);
+while(threeDiggit>1000)
+{
+    threeDiggit = threeDiggit/10;
+}
+if(threeDiggit>99)
+{
+    threeDiggit=threeDiggit%10;
+    Console.WriteLine("Третья цифра случайно выбранного числа из заданного диапозона это:");
+    Console.WriteLine(threeDiggit);
+}
+else
+{
+    Console.WriteLine("У значения нет третьего символа");
+}
+
+
