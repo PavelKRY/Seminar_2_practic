2e98188172856302f13c16fbaf8df1ccbf6be501
PavelKRY <kryukov.p14@gmail.com>
2022-08-26 13:01:00 +0500

    Task 02* issue

---
+++ Seminar_02_Task_02_01/Program.cs
@@ -0,0 +1,6 @@
+﻿int firstDiggit = new Random().Next(1,10000000);
+string firstDiggit_01 = firstDiggit.ToString();
+char threeDiggit = firstDiggit_01[2];
+Console.WriteLine(firstDiggit_01);
+Console.WriteLine("Третья цифра случайно выбранного числа из заданного диапозона это: ");
+Console.WriteLine(threeDiggit);
